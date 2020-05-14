c2920e998a1b6ffd41197613fc0074d3e181bc38
Umed Jadhav <umed@browserstack.com>
2020-05-14 08:06:51 +0530

    Created login,logout  pages with basic setup for profile page

--- djangoProj/urls.py
+++ djangoProj/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users.views import register
+from django.contrib.auth import views as auth_views
+from users.views import register, profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
     path('register/',register, name='register'),
+    path('profile/',profile, name='profile'),
+    path('login/',auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
 ]
